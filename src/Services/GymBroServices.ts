eb4d956f1ad28bc49e1c5843cf83f1f889cef68f
frediej <55649603+frediej@users.noreply.github.com>
2023-12-10 19:05:49 +0700

    Completed logic for matches view. Project is missing styling and unit tests. Will complete unit tests and get it deployed on render.

--- src/Services/GymBroServices.ts
+++ src/Services/GymBroServices.ts
@@ -1,17 +1,17 @@
 import {db} from '@/js/firebase';
-import {collection, doc, getDocs, setDoc} from "firebase/firestore";
+import {collection, doc, getDoc, getDocs, setDoc} from "firebase/firestore";
 
 const userColRef = collection(db, "users");
 
 const GymBroServices = {
     async getAllUsers() {
         const snapshot = await getDocs(userColRef);
-        return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        return snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
     },
     async addMatch(currentUserId: string, matchedUserId: string) {
         try {
             const matchRef = doc(db, "users", currentUserId, "matches", matchedUserId);
-            await setDoc(matchRef, { matched: true });
+            await setDoc(matchRef, {matched: true});
         } catch (error) {
             console.error("Error adding match:", error);
             throw error; // Throw error to handle it in the calling function
@@ -20,7 +20,7 @@ const GymBroServices = {
     async addReject(currentUserId: string, rejectedUserId: string) {
         try {
             const rejectRef = doc(db, "users", currentUserId, "rejects", rejectedUserId);
-            await setDoc(rejectRef, { rejected: true });
+            await setDoc(rejectRef, {rejected: true});
         } catch (error) {
             console.error("Error adding reject:", error);
             throw error; // Throw error to handle it in the calling function
@@ -36,7 +36,20 @@ const GymBroServices = {
             console.error("Error getting excluded users:", error);
             throw error; // Throw error to handle it in the calling function
         }
-    }
+    },
+    async getMatchedUsers(currentUserId: string) {
+        try {
+            const matchesRef = collection(db, "users", currentUserId, "matches");
+            const matchesSnapshot = await getDocs(matchesRef);
+            const matchIds = matchesSnapshot.docs.map(doc => doc.id);
+
+            const userDocs = await Promise.all(matchIds.map(userId => getDoc(doc(db, "users", userId))));
+            return userDocs.filter(doc => doc.exists()).map(doc => ({ id: doc.id, ...doc.data() }));
+        } catch (error) {
+            console.error("Error getting matched users:", error);
+            throw error;
+        }
+    },
 };
 
 export default GymBroServices;
